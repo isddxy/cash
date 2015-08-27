7bde0dec8524d340f1d4783e4d3d97726e6e15d9
Антон Миляев <isddxy@gmail.com>
2015-08-27 18:54:45 +0300

    Добавил Главный php файл

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+
+$char_1 = $_COOKIE['char_1'];
+$rate_1 = $_COOKIE['rate_1'];
+		
+include ("bd.php");
+
+$query = "SELECT * FROM `money` WHERE `char` = '$char_1' ";
+$result = mysql_query($query);
+$row = mysql_fetch_array($result);
+
+
+
+
+$rate_2 = $row['rate'];
+
+$sum = $rate_1 * $rate_2;
+
+setcookie("rate_2",$sum,time()+3600);
+
+echo $char_1."<br>";
+echo $row['rate'].'<br>';
+echo $sum;	
+
+?>
